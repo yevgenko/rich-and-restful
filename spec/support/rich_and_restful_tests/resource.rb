ad0c1bde62f15bb20bf78cb3df495208868028b5
Yevhen V <wik@bdhr.co>
2018-08-08 01:54:31 +0100

    Extract resource

---
+++ spec/support/rich_and_restful_tests/resource.rb
@@ -0,0 +1,53 @@
+module RichAndRestfulTests
+  class Resource
+    include Rails.application.routes.url_helpers
+
+    def initialize(*models)
+      default_url_options[:only_path] = true
+      @models = models
+      @model = models.last
+    end
+
+    def location
+      url_for @models
+    end
+
+    def payload
+      controller_name = Rails.application.routes.recognize_path(
+        location, method: :post
+      )[:controller]
+      controller = controller_class_for(controller_name).new
+      resource_name = @model.class.name.underscore
+
+      controller.render_to_string(
+        "_#{resource_name}".to_sym,
+        locals: { "#{resource_name}": @model }
+      )
+    end
+
+    def errors
+      @model.valid?
+      { :errors => @model.errors }.to_json
+    end
+
+    def headers
+      {
+        "CONTENT_TYPE" => 'application/json',
+        "ACCEPT" => "application/json"
+      }
+    end
+
+    private
+
+    # from actionpack/lib/action_dispatch/http/request.rb
+    def controller_class_for(name)
+      if name
+        controller_param = name.underscore
+        const_name = "#{controller_param.camelize}Controller"
+        ActiveSupport::Dependencies.constantize(const_name)
+      else
+        PASS_NOT_FOUND
+      end
+    end
+  end
+end
--- spec/support/rich_and_restful_tests/rest_client.rb
+++ spec/support/rich_and_restful_tests/rest_client.rb
@@ -5,37 +5,15 @@ module RichAndRestfulTests
     end
 
     def make_request(*resources)
-      resource = resources.last
-      url = @session.url_for(resources)
-      controller_name = Rails.application.routes.recognize_path(url, method: :post)[:controller]
-      controller = controller_class_for(controller_name).new
-      resource_name = resource.class.name.underscore
-      resource_json = controller.render_to_string(
-        "_#{resource_name}".to_sym,
-        locals: { "#{resource_name}": resource }
+      resource = Resource.new(*resources)
+
+      @session.post(
+        resource.location,
+        params: resource.payload,
+        headers: resource.headers
       )
 
-      headers = {
-        "CONTENT_TYPE" => 'application/json',
-        "ACCEPT" => "application/json"
-      }
-
-      @session.post url, params: resource_json, headers: headers
-
-      resource_json # request body
-    end
-
-    private
-
-    # from actionpack/lib/action_dispatch/http/request.rb
-    def controller_class_for(name)
-      if name
-        controller_param = name.underscore
-        const_name = "#{controller_param.camelize}Controller"
-        ActiveSupport::Dependencies.constantize(const_name)
-      else
-        PASS_NOT_FOUND
-      end
+      resource.payload # request body
     end
   end
 end
