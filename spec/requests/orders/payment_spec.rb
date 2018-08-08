c646aebc2da69cc2249f2d0b66e566166d3b3ac3
Yevhen V <wik@bdhr.co>
2018-08-08 02:54:30 +0100

    Extract rest client module

--- spec/requests/orders/payment_spec.rb
+++ spec/requests/orders/payment_spec.rb
@@ -11,35 +11,9 @@ RSpec.describe "POST /orders/:id/payments", type: :request do
     expect(client).to have_received_created_resource order, new_payment
   end
 
-  module RestClient
-    def create_resource(*models)
-      resource = resource_for models
-      post(
-        resource.location,
-        params: resource.payload,
-        headers: resource.headers
-      )
-    end
-
-    def has_received_created_resource?(*models)
-      resource = resource_for models
-      # TODO: why 200?
-      # expect(response).to have_http_status(:created)
-      expect { JSON.parse(response.body) }.to_not raise_error, response.body
-      expect(response.body).to be_json_eql resource.payload
-      expect(parse_json(response.body)).to include(
-        "id" => a_kind_of(Integer)
-      )
-    end
-
-    def resource_for(models)
-      RichAndRestfulTests::Resource.new(models)
-    end
-  end
-
   def a_client
     open_session do |session|
-      session.extend(RestClient)
+      session.extend(RichAndRestfulTests::RestClient)
     end
   end
 end
